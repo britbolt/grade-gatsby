fba3742d6da09eeeb5f792bee83db3eda63b6565
britbolt <blattisaw@gmail.com>
2022-05-26 14:22:45 -0400

    complete merge feature branch

--- server.js
+++ server.js
@@ -1,18 +1,47 @@
-const express = require('express');
-const sequelize = require('./config/connection');
-const { Teacher, Student, Subject } = require('./models');
-
-
+const express = require("express");
+const path = require("path");
+const { engine } = require("express-handlebars");
+const PORT = process.env.PORT || 3001;
+const sequelize = require("./config/connection");
+const { Teacher, Student, Subject } = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.set("view engine", "handlebars");
+
+app.engine(
+  "handlebars",
+  engine({
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+
+app.use(express.static(path.join(__dirname)));
+app.use(express.static("files"));
+app.use(express.json());
+
+app.post("api/login", (req, res) => {
+  const name = req.body.name,
+    email = req.body.email,
+    password = req.body.password;
+});
+
+app.get("/form", (req, res) => res.render("form"));
+app.get("/", (req, res) => {
+  res.render("layouts/index");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login-teacher", (req, res) => {
+  res.render("login-teacher");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 sequelize.sync({ force: true }).then(() => {
-    console.log("Database connected")
-    app.listen(PORT, () => console.log('Now listening'));
+  console.log("Database connected");
+  app.listen(PORT, () => console.log("Now listening"));
 });
-  
