09260aa50d397c7ab703ffb905ab9f62545a8275
Dave Sanders <davepaulsanders@gmail.com>
2022-05-27 09:54:03 -0400

    Post routes created for Student, Grade, and Teacher

--- controllers/api/gradeRoutes.js
+++ controllers/api/gradeRoutes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const Grade = require("../../models/Grade");
 
-
-
-
+// add a single grade
+router.post("/", async (req, res) => {
+  try {
+    const newGrade = await Grade.create({
+      grade: req.body.grade,
+      student_id: req.body.studentId,
+      subject_id: req.body.subjectId,
+    });
+    res.json(newGrade);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const gradeRoutes = require('./gradeRoutes')
 const studentRoutes = require('./studentRoutes')
 const teacherRoutes = require('./teacherRoutes')
 
-app.use('/grades', gradeRoutes)
-app.use('/student', studentRoutes)
-app.use('/teacher', teacherRoutes)
+router.use('/grades', gradeRoutes)
+router.use('/student', studentRoutes)
+router.use('/teacher', teacherRoutes)
 
 module.exports = router;
