a0c9cd12856b7c9130826c7220a5df87e34621f7
Dave Sanders <davepaulsanders@gmail.com>
2022-05-29 09:23:05 -0400

    Student login and sign up routes created

--- controllers/api/studentRoutes.js
+++ controllers/api/studentRoutes.js
@@ -1,8 +1,40 @@
 const router = require("express").Router();
 const Student = require("../../models/Student");
 
-// Add a single student
-router.post("/", async (req, res) => {
+router.post("/login", async (req, res) => {
+  try {
+    const loggedInStudent = await Student.findOne({
+      email: req.body.email,
+    });
+
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+    req.session.save(() => {
+      (req.session.user_id = newStudent.id),
+        (req.session.username = newStudent.email),
+        (req.session.loggedIn = true);
+      res.send({
+        student: loggedInStudent,
+        message: "New account created, logged in!",
+      });
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+  }
+});
+
+// Add a single student/sign up
+router.post("/signup", async (req, res) => {
   try {
     const newStudent = await Student.create({
       name: req.body.name,
@@ -10,7 +42,15 @@ router.post("/", async (req, res) => {
       password: req.body.password,
       teacher_id: req.body.teacherId,
     });
-    res.json(newStudent);
+    req.session.save(() => {
+      (req.session.user_id = newStudent.id),
+        (req.session.username = newStudent.email),
+        (req.session.loggedIn = true);
+      res.send({
+        student: newStudent,
+        message: "New account created, logged in!",
+      });
+    });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
