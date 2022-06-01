accd3bc20c9dc52f70e8ca03ee71d11a50156f45
Dave Sanders <davepaulsanders@gmail.com>
2022-05-31 20:53:35 -0400

    Login and sign up connected to dashboards

--- controllers/api/teacherRoutes.js
+++ controllers/api/teacherRoutes.js
@@ -3,69 +3,60 @@ const Teacher = require("../../models/Teacher");
 
 // Add a single teacher
 router.post("/signup", async (req, res) => {
-    try {
-      const newTeacher = await Teacher.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      });
 
-      // not sure if teacher id and username is approprite for this
-      // just followed module so ¯\_(ツ)_/¯
-      req.session.save(() => {
-        req.session.teacher_id = newTeacher.id;
-        req.session.username = newTeacher.username;
-        req.session.loggedIn = true;
-  
-        res.redirect('/dashboard/teacher');
-        })
-
-// where does this res.json go??
-// jk only works with res.json commented out, with res.redirect instead
-      // res.json(newTeacher);
-    } catch (err) {
-      console.log(err.message);
-      res.sendStatus(500)
-    }
-  });
-
-  // teacher login
-router.post("/login", (req, res) => {
-  console.log(req.body.email);
-  Teacher.findOne ({
-    where: {
-      email: req.body.email
-    }
-  })
-  .then(teacherLoginData => {
-    console.log(teacherLoginData);
-    if(!teacherLoginData) {
-      res.status(400).json({ message: 'Invalid email' });
-      return;
-    }
-
-    const validPassword = teacherLoginData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res.status(400).json({ message: 'Invalid password' });
-      return;
-    }
-
+  try {
+    const newTeacher = await Teacher.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
     req.session.save(() => {
-      req.session.teacher_id = teacherLoginData.id;
-      req.session.username = teacherLoginData.username;
+      req.session.teacher_id = newTeacher.id;
+      req.session.username = newTeacher.username;
       req.session.loggedIn = true;
 
-      res.redirect('/dashboard/teacher');
-
-      // res.json({ user: teacherLoginData, message: 'Logged in' });
+      res.send({ user: newTeacher, message: "Signed up" });
     });
-  })
-  .catch(err => {
-    console.log(err);
+  } catch (err) {
+    console.log(err.message);
     res.sendStatus(500);
-  })
+  }
 });
 
+// teacher login
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  Teacher.findOne({
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then((teacherLoginData) => {
+      console.log(teacherLoginData);
+      if (!teacherLoginData) {
+        res.status(400).json({ message: "Invalid email" });
+        return;
+      }
+
+      const validPassword = teacherLoginData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res.status(400).json({ message: "Invalid password" });
+        return;
+      }
+
+      req.session.save(() => {
+        req.session.teacher_id = teacherLoginData.id;
+        req.session.username = teacherLoginData.username;
+        req.session.loggedIn = true;
+
+        res.send({ user: teacherLoginData, message: "Logged in" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,10 +4,10 @@ router.get("/teacher", (req, res) => {
   if(!req.session.loggedIn) {
     res.redirect('/');
   }
-  res.render("login-teacher");
+  res.render("teacher-dashboard");
 });
 router.get("/student", (req, res) => {
-  res.render("");
+  res.render("student-dashboard");
 });
 
 module.exports = router;
