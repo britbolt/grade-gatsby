faaec0f2af87fa310722673d33f0d2edf036fc08
britbolt <blattisaw@gmail.com>
2022-06-04 00:43:05 -0400

    responsive table design for teacher dash

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -6,7 +6,7 @@ console.log('load!!');
 
   $('.table-add').on("click", function () {
     console.log('add');
-    var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+    var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide');
     console.log($clone);
     $TABLE.find('table').append($clone);
   });
@@ -15,38 +15,6 @@ console.log('load!!');
     console.log('remove');
     $(this).parents('tr').detach();
   });
-
-
-  // jQuery helpers for exporting
-  jQuery.fn.pop = [].pop;
-  jQuery.fn.shift = [].shift;
-
-  $BTN.on("click", function () {
-    console.log('click');
-    var $rows = $TABLE.find('tr:not(:hidden)');
-    var grades = [$rows.find('td:not(:hidden)')];
-    var students = [$rows.find('th.student-name:not(:hidden)')];
-
-
-    // Get the subjects
-    $($rows.shift()).find('th:not(:empty)').each(function () {
-      grades.push($(this).text().toLowerCase());
-    });
-
-    // Turn all existing rows into a loopable array
-    $rows.each(function () {
-      var $td = $(this).find('td');
-      var h = {};
-
-      grades.forEach(function (grades, i) {
-        h[grades] = $td.eq(i).text();
-      });
-
-      students.push(h);
-    });
-
-    // Output the result to page for now, need to update to output to update db
-    $SAVE.text(JSON.stringify(students));
-  });
 });
+
 
